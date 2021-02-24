40edfc0359899923b88ae927fb7e93e9635589fd
Vitor Barbosa <vitor.barbosa@accountfy.com>
2021-02-24 19:35:29 -0300

    criação de tabela por migrations

---
+++ rocketpay/priv/repo/migrations/20210223233142_create_user_table.exs
@@ -0,0 +1,18 @@
+defmodule Rocketpay.Repo.Migrations.CreateUserTable do
+  use Ecto.Migration
+
+  def change do
+    create table :user do
+      add :name, :string
+      add :age, :integer
+      add :email, :string
+      add :password_hash, :string
+      add :nickname, :string
+
+      timestamps()
+    end
+
+    create unique_index(:user, [:email])
+    create unique_index(:user, [:nickname])
+  end
+end
